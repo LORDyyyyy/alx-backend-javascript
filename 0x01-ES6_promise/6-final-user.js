41173612b1b3f3462006bf5ce9a9366a7324c28d
LORDyyyyy <mlord6271@gmail.com>
2024-01-04 16:19:22 +0200

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((data) => {
-      const result = [];
+      let result = [];
       result = data.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : result.reason
