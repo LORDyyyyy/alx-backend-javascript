b1d6d36290d2b804a6bb194401bbebfa3a203708
LORDyyyyy <mlord6271@gmail.com>
2024-02-27 23:13:49 +0200

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+let data = null;
+function countStudents(path) {
+  try {
+    data = fs.readFileSync(path, 'utf-8');
+  } catch (err) {
+    console.log('Cannot load the database');
+    process.exit();
+  }
+  data = data.split('\n').filter((line) => line.trim() !== '');
+  data.shift();
+  const numberOfStd = data.length;
+  const fields = {};
+
+  for (const i in data) {
+    const line = data[i].split(',');
+    if (!fields[line[3]]) {
+      fields[line[3]] = [];
+    }
+    fields[line[3]].push(line[0]);
+  }
+  console.log(`Number of students: ${numberOfStd}`);
+  Object.keys(fields).forEach((field) => {
+    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  });
+}
+
+module.exports = countStudents;
