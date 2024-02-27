230aa253975f42951fa14702024e38072227492c
LORDyyyyy <mlord6271@gmail.com>
2024-02-27 20:52:55 +0200

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (!process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}\n`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
   });
 
@@ -12,6 +12,6 @@ if (!process.stdin.isTTY) {
 }
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.stdout.write(`Your name is: ${data.toString()}`);
   process.exit();
 });
