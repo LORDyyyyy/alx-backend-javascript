0e768e4e4f86505995b806d2c98d02840d7d63da
LORDyyyyy <mlord6271@gmail.com>
2024-03-01 01:28:58 +0200

    task 6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+const sinon = require('sinon');
+const assert = require('assert');
+const getPaymentTokenFromAPI = require('./6-payment_token.js')
+
+describe('getPaymentTokenFromAPI', () => {
+    it('should resolve with successful response when success is true', (done) => {
+        getPaymentTokenFromAPI(true).then((response) => {
+            assert.deepStrictEqual(response, { data: 'Successful response from the API' });
+            done();
+        }).catch(error => {
+            done(error);
+        });
+    });
+});
